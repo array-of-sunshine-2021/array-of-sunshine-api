68a7b74fcbe53b9d5a847717aaa723a5d5acfa4a
Tobi <tobi.daniel2018@gmail.com>
2021-03-13 17:05:35 -0500

    burh

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
-import './styles/App.scss';
-import axios from 'axios';
-import { useState } from 'react';
-import Form from './Form'
+import "./styles/App.scss";
+import axios from "axios";
+import { useState } from "react";
+import Form from "./Form";
 
 function App() {
   //define state for the form inputs
-  const [dateInput, setDateInput] = useState('');
-  
+  const [dateInput, setDateInput] = useState("");
+
   //create functions to handle input changes from the form
   const handleDateInput = (event) => {
-    const date = event.target.value 
-    setDateInput(date)
-  }
+    const date = event.target.value;
+    setDateInput(date);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     axios({
-      method: 'GET',
-      url: 'https://api.sunrise-sunset.org/json',
+      method: "GET",
+      url: "https://api.sunrise-sunset.org/json",
       params: {
-        lat: 36.7201600,
-        lng: -4.4203400,
-        date: `${dateInput}`
-      }
-    }).then(response => {
-      response = response.data.results
+        lat: 36.72016,
+        lng: -4.42034,
+        date: `${dateInput}`,
+      },
+    }).then((response) => {
+      response = response.data.results;
       console.log(response);
-    })
-  }
-
+    });
+  };
 
   return (
     <div className="App">
       <h1>SUN RUN</h1>
       <h2>hello, testing</h2>
       <Form
-      submit={handleSubmit}
-      date= {dateInput}
-      dateChange={handleDateInput}
+        submit={handleSubmit}
+        date={dateInput}
+        dateChange={handleDateInput}
       />
-      
     </div>
   );
 }
