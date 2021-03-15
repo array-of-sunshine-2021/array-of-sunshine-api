45e4637cb3f14f28e577cd04ae64b8b7804dc875
Tobi <tobi.daniel2018@gmail.com>
2021-03-15 17:10:44 -0400

    error handling

--- src/App.js
+++ src/App.js
@@ -21,25 +21,22 @@ function App() {
         timezone: 'eastern',
         lat: 43.6532,
         lng: -79.3832,
-        timezone: 'eastern',
+        
       },
     {
         timezone: 'central',
         lat: 29.7604,
         lng: -95.3698,
-        timezone: 'central',
       },
     {
       timezone: 'pacific',
       lat:  37.7749,
       lng:  -122.4194,
-      timezone: 'pacific',
     },
     {
       timezone: 'mountain',
       lat:  33.4484,
       lng:  -112.0740,
-      timezone: 'mountain',
     }
   ]
 
--- src/Results.js
+++ src/Results.js
@@ -1,34 +1,37 @@
 //Results.js
 
-const Results = ({apiResult , userDuration , timeZone}) => {
-     const sunrise = apiResult.sunrise
-     const sunset = apiResult.sunset 
-     console.log(apiResult)
-     if(timeZone === 'eastern' ){
-          console.log(sunrise)
-          const covertedESTRise = parseInt(sunrise) - 8;
-          const covertedESTSet = parseInt(sunset) - 4;
-          console.log(covertedESTRise)
+const Results = ({ apiResult, userDuration, timeZone }) => {
+  const sunriseApi = apiResult.sunrise;
+  const sunsetApi = apiResult.sunset;
+  console.log(apiResult);
+  if (timeZone === "eastern") {
+       //     console.log(typeof sunrise);
+       const sunriseSubstrEst = sunriseApi.substr(2);
+console.log(sunriseSubstrEst)
+//     const covertedESTRise = parseInt(sunrise) - 4;
+//     const covertedESTSet = parseInt(sunset) - 4;
 
-     }else if (timeZone === 'pacific') {
-          const covertedPST = parseInt(sunrise) + 5;
-          const covertedPSTSet = parseInt(sunset) - 4;
+//     const sunriseSubstrEst = sunrise.subs?t(2);
+     //    console.log(typeof(covertedESTRise));
+  } else if (timeZone === "central") {
+    const covertedCTRise = parseInt(sunriseApi) - 5;
+    const covertedCTSet = parseInt(sunsetApi) - 5;
+    //     console.log(covertedCT);
+  } else if (timeZone === "mountain") {
+    const covertedMTRise = parseInt(sunriseApi) - 7;
+    const covertedMTSet = parseInt(sunsetApi) - 7;
+    //     console.log(covertedMT);
+  } else if (timeZone === "pacific") {
+    const covertedPSTRise = parseInt(sunriseApi) - 7;
+    const covertedPSTSet = parseInt(sunsetApi) - 7;
 
-          console.log(covertedPST)
+    //     console.log(covertedPST);
+  }
 
-     }else if (timeZone === 'mountain') {
-          const covertedMT = parseInt(sunrise) + 7;
-          console.log(covertedMT)
+  // const newSunriseTime = parseInt(sunrise) + 4
+  // console.log(newSunriseTime)
 
-     }else if (timeZone === 'central'){
-          const covertedCT = parseInt(sunrise) + 7;
-          console.log(covertedCT)
-     }
-
-     // const newSunriseTime = parseInt(sunrise) + 4
-     // console.log(newSunriseTime)
-
-     // const testing = () => {
+  // const testing = () => {
   //   const test = '12:45:10 AM'
 
   //   console.log(typeof(test.substring(2)))
@@ -37,11 +40,7 @@ const Results = ({apiResult , userDuration , timeZone}) => {
   // }
   // testing()
 
-     return(
-          <div>
-
-          </div>
-     )
-}
+  return <div></div>;
+};
 
 export default Results;
