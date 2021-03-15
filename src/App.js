a04376991087ff809e345b903c533b0e7336dde0
Tobi <tobi.daniel2018@gmail.com>
2021-03-15 16:01:06 -0400

    styling page

--- src/App.js
+++ src/App.js
@@ -2,38 +2,38 @@ import "./styles/App.scss";
 import axios from "axios";
 import { useState } from "react";
 import Form from "./Form";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRunning } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   //define state for the form inputs
-  const [dateInput, setDateInput] = useState('');
-  const [sunrise , setSunrise] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-
+  const [dateInput, setDateInput] = useState("");
+  const [sunrise, setSunrise] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
 
   const timeZoneCoordinates = [
     {
-        timezone: 'eastern',
-        lat: 43.6532,
-        lng: 79.3832
-      },
-    {
-        timezone: 'central',
-        lat:29.7604,
-        lng:95.3698,
-      },
-    {
-      timezone: 'pacific',
-      lat:37.7749,
-      lng:122.4194,
+      timezone: "eastern",
+      lat: 43.6532,
+      lng: 79.3832,
     },
     {
-      timezone: 'mountain',
-      lat:33.4484,
-      lng:112.0740,
-    }
-  ]
+      timezone: "central",
+      lat: 29.7604,
+      lng: 95.3698,
+    },
+    {
+      timezone: "pacific",
+      lat: 37.7749,
+      lng: 122.4194,
+    },
+    {
+      timezone: "mountain",
+      lat: 33.4484,
+      lng: 112.074,
+    },
+  ];
 
   //create functions to handle input changes from the form
   const handleDateInput = (event) => {
@@ -42,29 +42,25 @@ function App() {
   };
 
   const handleTimeZone = (event) => {
-    const timeZoneSelected = event.target.value
-    
+    const timeZoneSelected = event.target.value;
 
-    const timeZoneUserChoice = timeZoneCoordinates.filter((timeZoneCoordinate) => {
-      // console.log(timeZoneCoordinate)
-      return(
-        timeZoneSelected === timeZoneCoordinate.timezone
-      )
-    })
-    console.log(timeZoneUserChoice)
-    const latitude = timeZoneUserChoice[0].lat
-    const longitude= timeZoneUserChoice[0].lng
-    console.log(latitude, longitude)
-    setLatitude(latitude)
-    setLongitude(longitude)
-  }
-
+    const timeZoneUserChoice = timeZoneCoordinates.filter(
+      (timeZoneCoordinate) => {
+        // console.log(timeZoneCoordinate)
+        return timeZoneSelected === timeZoneCoordinate.timezone;
+      }
+    );
+    console.log(timeZoneUserChoice);
+    const latitude = timeZoneUserChoice[0].lat;
+    const longitude = timeZoneUserChoice[0].lng;
+    console.log(latitude, longitude);
+    setLatitude(latitude);
+    setLongitude(longitude);
+  };
 
   const handleRadioChoice = (event) => {
-    const radioChoice = event.target.value
-
-    
-  }
+    const radioChoice = event.target.value;
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -82,21 +78,22 @@ function App() {
     });
   };
 
-
   return (
     <div className="App">
-      <h1>SUN RUN</h1>
-      <h2>hello, testing</h2>
+      <header>
+        <h1 className="wrapper">
+          SUNRUN <FontAwesomeIcon icon={faRunning} />
+        </h1>
+      </header>
       <Form
-      submit={handleSubmit}
-      date= {dateInput}
-      dateChange={handleDateInput}
-      timeZone = {handleTimeZone}
-      sunRun = {handleRadioChoice}
+        submit={handleSubmit}
+        date={dateInput}
+        dateChange={handleDateInput}
+        timeZone={handleTimeZone}
+        sunRun={handleRadioChoice}
       />
     </div>
   );
 }
 
 export default App;
-
