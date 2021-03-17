b1d35473d046318ca1bae26a2a88c76096750225
jordanchan <jordancmchan@hotmail.com>
2021-03-17 19:42:10 -0400

    fixed issue

--- src/Results.js
+++ src/Results.js
@@ -24,38 +24,41 @@ const Results = ({ apiResult , timeZone}) => {
       if(timeZone === "America/Toronto"){
         const sunRiseTime = moment(`${sunriseApi}`);
         const finalSunRiseTime = sunRiseTime.tz("America/Toronto").format('HH:mm:ss A')
-        // console.log(anotherTime)
         setTimeZoneSelected(finalSunRiseTime)
 
         const sunSetTime = moment(`${sunsetApi}`)
         const finalSunSetTime = sunSetTime.tz("America/Toronto").format('HH:mm:ss A')
+
         setSunsetTimeZoneSlected(finalSunSetTime)
 
 
 
       }else if(timeZone === "America/Rainy_River"){
+
         const sunRiseTime = moment(`${sunriseApi}`);
         const finalSunRiseTime = sunRiseTime.tz("America/Rainy_River").format('HH:mm:ss A')
         setTimeZoneSelected(finalSunRiseTime)
-        // console.log(anotherTime)
+
         const sunSetTime = moment(`${sunsetApi}`)
         const finalSunSetTime = sunSetTime.tz("America/Rainy_River").format('HH:mm:ss A')
         setSunsetTimeZoneSlected(finalSunSetTime)
 
       }else if(timeZone === "America/Denver"){
+
         const sunRiseTime = moment(`${sunriseApi}`);
         const finalSunRiseTime = sunRiseTime.tz("America/Denver").format('HH:mm:ss A')
         setTimeZoneSelected(finalSunRiseTime)
-        // console.log(anotherTime)
+
         const sunSetTime = moment(`${sunsetApi}`)
         const finalSunSetTime = sunSetTime.tz("America/Denver").format('HH:mm:ss A')
         setSunsetTimeZoneSlected(finalSunSetTime)
 
       }else if(timeZone === "America/Vancouver"){
+
         const sunRiseTime = moment(`${sunriseApi}`);
         const finalSunRiseTime = sunRiseTime.tz("America/Vancouver").format('HH:mm:ss A')
         setTimeZoneSelected(finalSunRiseTime)
-        // console.log(anotherTime)
+        
         const sunSetTime = moment(`${sunsetApi}`)
         const finalSunSetTime = sunSetTime.tz("America/Vancouver").format('HH:mm:ss A')
         setSunsetTimeZoneSlected(finalSunSetTime)
@@ -83,7 +86,8 @@ const Results = ({ apiResult , timeZone}) => {
           <p>{sunsetTimeZonSelected}</p>
           {
             sunsetTimeZonSelected
-              ? <SunsetForm sunSetTime = {timeZoneSelected}/>
+              ? <SunsetForm 
+              sunSetTime = {sunsetTimeZonSelected}/>
               : ''
           }
         </div>
