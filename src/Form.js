f141082da4d4c2520af5433ecf6abcba5d5cb5fb
Tobi <tobi.daniel2018@gmail.com>
2021-03-17 12:20:55 -0400

    Updated the page styling

--- src/Form.js
+++ src/Form.js
@@ -1,35 +1,45 @@
 // Form.js
-import { useState } from 'react';
-
+import { useState } from "react";
 
 const Form = (props) => {
-    const [sunSet, setSunSet] = useState('false');
-    
-    const handleToggle = () => {
-        setSunSet(!sunSet)
-    }
+  const [sunSet, setSunSet] = useState("false");
 
-    const handleToggleSunrise = () => {
-        setSunSet('false')
-    }
+  const handleToggle = () => {
+    setSunSet(!sunSet);
+  };
 
-    return (
-        <form action="" className="wrapper">
-            <label htmlFor="dateInput">What day will you go for a run?</label>
-            <input type="date" id="dateInput" className="dateInput" value={props.dateInput} onChange={props.dateChange} />
+  const handleToggleSunrise = () => {
+    setSunSet("false");
+  };
 
-            <label htmlFor="timeZonesInput">Pick your timezone:</label>
-            <select name="timeZonesInput" id="timeZonesInput" className="timeZoneInput" onChange ={props.timeZone} >
-                <option value="eastern">Eastern</option>
-                <option value="central">Central</option>
-                <option value="pacific">Pacific</option>
-                <option value="mountain">Mountain</option>
-            </select>
-            <button type="submit" onClick={props.submit}>Let's Workout!</button>
+  return (
+    <form action="" className="wrapper">
+      <label htmlFor="dateInput">What day will you go for a run?</label>
+      <input
+        type="date"
+        id="dateInput"
+        className="dateInput"
+        value={props.dateInput}
+        onChange={props.dateChange}
+      />
 
-
-        </form>
-    )
+      <label htmlFor="timeZonesInput">Pick your timezone:</label>
+      <select
+        name="timeZonesInput"
+        id="timeZonesInput"
+        className="timeZoneInput"
+        onChange={props.timeZone}
+      >
+        <option value="eastern">Eastern</option>
+        <option value="central">Central</option>
+        <option value="pacific">Pacific</option>
+        <option value="mountain">Mountain</option>
+      </select>
+      <button type="submit" onClick={props.submit}>
+        Let's Workout!
+      </button>
+    </form>
+  );
 };
 
 export default Form;
